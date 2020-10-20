5927ccb1d0cee15f0f5e21f49c0e9909340e22aa
Symphysis <alex.rynkowski90@gmail.com>
2020-10-20 15:01:39 +0200

    Hud now required gold and goldpress compomnents

--- Assets/Scripts/Hud/Hud.cs
+++ Assets/Scripts/Hud/Hud.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace Hud
 {
+    [RequireComponent(typeof(Gold)), RequireComponent(typeof(GoldPress))]
     public class Hud : MonoBehaviour
     {
         [SerializeField] float productionTime;
