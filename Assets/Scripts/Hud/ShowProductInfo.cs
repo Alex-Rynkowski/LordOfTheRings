6c2a17b69abab4a39accf58f5caa5def30a32404
Symphysis <alex.rynkowski90@gmail.com>
2020-10-20 16:39:42 +0200

    adds show / hide product info

---
+++ Assets/Scripts/Hud/ShowProductInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Hud
+{
+    public class ShowProductInfo : MonoBehaviour, IPointerClickHandler
+    {
+        [SerializeField] GameObject infoField;
+
+        public bool DoShowProductInfo { get; set; }
+
+        void Start()
+        {
+            infoField.SetActive(DoShowProductInfo = false);
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (DoShowProductInfo)
+            {
+                infoField.SetActive(DoShowProductInfo = false);
+                return;
+            }
+
+            infoField.SetActive(DoShowProductInfo = true);
+        }
+    }
+}
