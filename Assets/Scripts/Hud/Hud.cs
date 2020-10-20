9e634e01f48990ecec58f39607da083367707961
Symphysis <alex.rynkowski90@gmail.com>
2020-10-20 10:13:27 +0200

    User can increment gold

---
+++ Assets/Scripts/Hud/Hud.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Hud
+{
+    public class Hud : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI goldText;
+        [SerializeField] int playerGold;
+
+        int Gold
+        {
+            get => playerGold;
+            set => playerGold = value;
+        }
+
+        void Start()
+        {
+            GoldText();
+        }
+
+        void GoldText()
+        {
+            goldText.text = $"Gold: {Gold}";
+        }
+
+        public void IncrementGold(int goldToIncrement)
+        {
+            Gold += 5;
+            GoldText();
+        }
+    }
+}
