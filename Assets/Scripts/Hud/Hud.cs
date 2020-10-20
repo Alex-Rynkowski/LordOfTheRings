72c9f7d78bd72c084abb5907b1bc25e8b2c613a4
Symphysis <alex.rynkowski90@gmail.com>
2020-10-20 15:58:36 +0200

    moves produce gold to a separate component

--- Assets/Scripts/Hud/Hud.cs
+++ Assets/Scripts/Hud/Hud.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace Hud
@@ -6,20 +7,8 @@ namespace Hud
     [RequireComponent(typeof(Gold)), RequireComponent(typeof(GoldPress))]
     public class Hud : MonoBehaviour
     {
-        [SerializeField] float productionTime;
-        [SerializeField] int productionAmount;
-        float _lastGoldIncrement;
-
-        bool GoldIncrementTimer => Time.time - this._lastGoldIncrement < productionTime;
-
         void Update()
         {
-            if (!GoldIncrementTimer)
-            {
-                GetComponent<Gold>().UpdateGold += GetComponent<GoldPress>().GoldPressesOwned + productionAmount;
-                this._lastGoldIncrement = Time.time;
-            }
-
             if (Input.GetMouseButtonDown(0))
             {
                 EventSystem.current.IsPointerOverGameObject();
---
+++ Assets/Scripts/Hud/ProduceGold.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Hud
+{
+    public class ProduceGold : MonoBehaviour
+    {
+        [SerializeField] float productionTime;
+        [SerializeField] int productionAmount;
+
+        float _lastGoldIncrement;
+        bool GoldIncrementTimer => Time.time - this._lastGoldIncrement < productionTime;
+
+        void Update()
+        {
+            if (!GoldIncrementTimer)
+            {
+                GetComponent<Gold>().UpdateGold += GetComponent<GoldPress>().GoldPressesOwned + productionAmount;
+                this._lastGoldIncrement = Time.time;
+            }
+        }
+    }
+}
