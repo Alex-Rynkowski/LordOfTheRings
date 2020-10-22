c14de3e4b00245fb3372c90f08e479a9cb127458
Alex <alex.rynkowski90@gmail.com>
2020-10-22 22:12:37 +0200

    reorganizes files

---
+++ Assets/Scripts/Units/WoodPress.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Hud
+{
+    public class WoodPress : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI woodPressText;
+        int _woodPressesOwned;
+
+        void Start()
+        {
+            HudServices.UpdateTextInfo(woodPressText, "WD owned:", UnitName.WoodPress);
+        }
+
+        public void WoodPressPurchase()
+        {
+            HudServices.PurchaseUnit(UnitName.WoodPress, 1.1f, RType.Wood);
+            HudServices.UpdateTextInfo(woodPressText, "WD owned:", UnitName.WoodPress);
+        }
+    }
+}
